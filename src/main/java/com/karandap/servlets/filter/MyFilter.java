d091920c6d7fc321b888ca29c6b938fa4f85c57d
Pavel Karanda <77533722+karanda-p@users.noreply.github.com>
2021-03-29 17:11:19 +0300

    Major update
    Adding auth filter

---
+++ src/main/java/com/karandap/servlets/filter/MyFilter.java
@@ -0,0 +1,47 @@
+package com.karandap.servlets.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Base64;
+
+@WebFilter("/books/*")
+public class MyFilter implements Filter {
+
+    private static final String USER = "user";
+    private static final String PASSWORD = "password";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+        String authorization = req.getHeader("Authorization");
+        if (authorization == null) {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+            return;
+        }
+        String formattedAuth = authorization.substring(6);
+        String authDecoded = new String(Base64.getDecoder().decode(formattedAuth));
+        String[] auth = authDecoded.split(":");
+        System.out.println(Arrays.toString(auth));
+        if (auth[0].equals(USER) && auth[1].equals(PASSWORD)) {
+            RequestDispatcher rd = req.getRequestDispatcher("/books/");
+            rd.include(req, resp);
+        } else {
+            resp.sendError(HttpServletResponse.SC_FORBIDDEN);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/main/java/com/karandap/servlets/servlet/MyServlet.java
@@ -0,0 +1,119 @@
+package com.karandap.servlets.servlet;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.karandap.servlets.dao.BookDAO;
+import com.karandap.servlets.dao.BookDAOImpl;
+import com.karandap.servlets.entity.Book;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/books/*")
+public class MyServlet extends HttpServlet {
+
+    private BookDAO bookDAO;
+
+    private ObjectMapper om;
+
+    public void init() throws ServletException {
+        bookDAO = new BookDAOImpl();
+        om = new ObjectMapper();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+//        String id = req.getParameter("id");
+        PrintWriter pw = resp.getWriter();
+        String path = req.getPathInfo();
+        List<Book> books = bookDAO.getAllBooks();
+        if (path == null || path.equals("/")) {
+            pw.print(om.writeValueAsString(books));
+            return;
+        }
+
+        String[] splits = path.split("/");
+
+        if (splits.length != 2) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        if (bookDAO.getBookById(Integer.parseInt(splits[1])) == null) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        pw.print(om.writeValueAsString(bookDAO.getBookById(Integer.parseInt(splits[1]))));
+        pw.flush();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String path = req.getPathInfo();
+        if (path == null || path.equals("/")) {
+            Book book = om.readValue(req.getReader(), Book.class);
+            bookDAO.updateBook(book);
+        } else {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getPathInfo();
+        List<Book> books = bookDAO.getAllBooks();
+        if (path == null || path.equals("/")) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+        String[] splits = path.split("/");
+
+        if (splits.length != 2) {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        if (bookDAO.getBookById(Integer.parseInt(splits[1])) == null) {
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        bookDAO.deleteBook(Integer.parseInt(splits[1]));
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String path = req.getPathInfo();
+        List<Book> books = bookDAO.getAllBooks();
+        if (path == null || path.equals("/")){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        String[] splits = path.split("/");
+
+        if (splits.length != 2){
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+            return;
+        }
+
+        if (bookDAO.getBookById(Integer.parseInt(splits[1])) == null){
+            resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+        Book book = om.readValue(req.getReader(), Book.class);
+        book.setId(Integer.parseInt(splits[1]));
+        bookDAO.updateBook(book);
+    }
+}
